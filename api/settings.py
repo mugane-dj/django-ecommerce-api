87ce61ca24d51b0c2d59904edbfbc7d86900b952
mugane-dj <muganes25@gmail.com>
2022-05-21 08:09:14 +0300

    initial commit

--- api/settings.py
+++ api/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
-
+#DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = ['https://django-ecomm-api.herokuapp.com/']
 
 # Application definition
