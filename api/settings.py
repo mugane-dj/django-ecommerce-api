b4ae9e99ad6df7214fa7c76923e6b81ae5e98978
mugane-dj <muganes25@gmail.com>
2022-05-24 12:31:44 +0300

    initial commit

--- api/settings.py
+++ api/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-USE_S3 = os.getenv('USE_S3')
+USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if USE_S3:
     # aws settings
