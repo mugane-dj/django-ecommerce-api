174895ae380fd72db723e75ca990124c4e989fd1
mugane-dj <muganes25@gmail.com>
2022-05-24 13:38:10 +0300

    add static file

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://django-ecomm-api.herokuapp.com/']
+ALLOWED_HOSTS = ['https://django-ecomm-api.herokuapp.com/', 'django-ecomm-api.herokuapp.com', 'www.django-ecomm-api.herokuapp.com']
 
 # Application definition
 
