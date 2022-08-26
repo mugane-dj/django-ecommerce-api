4ba5c6c3e6c10127483f780b8bbb5af6f108618e
mugane-dj <muganedev25@gmail.com>
2022-08-26 14:52:24 +0300

    cache config

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'TRUE'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://drf.azurewebsites.net/']
 # Application definition
 
 INSTALLED_APPS = [
